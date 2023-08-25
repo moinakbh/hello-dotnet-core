4a137ad18d258d5df77733d4d7215797085d8726
moinakbh <moinakb@gmail.com>
2023-08-25 12:37:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!" + System.DateTime.Now.ToString());
+Console.WriteLine("Hello, World! Note the date here -> " + System.DateTime.Now.ToString());
